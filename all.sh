32cb319e38cf167bb171b3ca4808ec3888543074
abrother94 <abrother94@gmail.com>
2019-05-29 13:39:14 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 ####################################
-rfip=172.17.8.122:8888
-listener=172.17.10.60:8889
-FirmwareVersion=2.1.3.59.21
+#
+# ${1} DUT's IP address.
+# ${2} Event listener's IP address.
+# ${3} PSME version want to test.
+#
+####################################
+
+if [ "${1}" == "" ];then
+    rfip=172.17.8.122:8888
+    echo "Use default target device IP ${rfip}"
+else
+    echo "Use target device IP ${1}"
+    rfip=${1}
+fi
+
+if [ "${2}" == "" ];then
+    listener=172.17.10.60:8889
+    echo "Use default listener IP ${listener}"
+else
+    echo "Use listener IP ${2}"
+    listener=172.17.10.60:8889
+fi
+
+if [ "${3}" == "" ];then
+    FirmwareVersion=2.1.3.59.21
+    echo "PSME FirmwareVersion ${FirmwareVersion}"
+else
+    FirmwareVersion=${3}
+    echo "PSME FirmwareVersion ${FirmwareVersion}"
+fi
+
+
 ####################################
 rm -rf log*;output.xml;report.html
 curl --insecure -X POST -D headers.txt https://${rfip}/redfish/v1/SessionService/ -d '{"ServiceEnabled":true,"SessionTimeout":600}'
 robot -v OPENBMC_HOST:${rfip} -v LISTENER_HOST:${listener} -v FWV:${FirmwareVersion} redfish/
+
